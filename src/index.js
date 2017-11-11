83df0a48d95249f2c871c3f591a8dba3eb7b2f14
arutyunyan_aa <artalar@ya.ru>
2017-11-11 15:29:42 +0300

    add create async cycle method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import { createAction } from 'redux-act';
-
 export const createAsyncAction = (description, asyncFunction) =>
   createAction(description, (...args) => ({
     ...args,
-    reduxActDispatchFreeAsyncAction: store => asyncFunction(store)(...args),
+    reduxActDispatchFreeAsyncAction: store => asyncFunction(store)(...args)
   }));
 
 export const asyncActionsCallerMiddleware = store => next => action => {
@@ -22,4 +20,22 @@ export const asyncActionsCallerMiddleware = store => next => action => {
     return next(action);
   }
   return next(action);
+};
+
+const createAsyncCycle = (description, asyncFunction) => {
+  const success = createAction(`[success] ${description}`);
+  const error = createAction(`[error] ${description}`);
+  const fetch = createAction(`[fetch] ${description}`, (...args) => ({
+    ...args,
+    reduxActDispatchFreeAsyncAction: async store => {
+      try {
+        const response = await asyncFunction(store)(...args);
+        success(response);
+      } catch (e) {
+        error(e);
+      }
+    },
+  }));
+
+  return [fetch, success, error];
 };
